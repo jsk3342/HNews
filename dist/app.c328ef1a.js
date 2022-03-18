a281fa96e207c81d181531d2bfabd647ed5bdd7a
jsk3342 <kss2460@naver.com>
2022-03-19 02:44:24 +0900

    코드 구조 단축화
    중복되는 코드 함수로 정의,
    DOM API 최대한 줄이기,
    구현 코드 알아볼 수 있도록 단축화

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -123,30 +123,31 @@ var container = document.getElementById("root");
 var content = document.createElement("div");
 var NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 var CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
-ajax.open("GET", NEWS_URL, false);
-ajax.send();
-var newsFeed = JSON.parse(ajax.response);
+
+function getData(url) {
+  ajax.open("GET", url, false);
+  ajax.send();
+  return JSON.parse(ajax.response);
+}
+
+var newsFeed = getData(NEWS_URL);
 var ul = document.createElement("ul");
 window.addEventListener("hashchange", function () {
   var id = location.hash.substring(1);
-  ajax.open("GET", CONTENT_URL.replace("@id", id), false);
-  ajax.send();
-  var newsContent = JSON.parse(ajax.response);
+  var newsContent = getData(CONTENT_URL.replace("@id", id));
   var title = document.createElement("h1");
-  title.innerHTML = newsContent.title;
-  content.appendChild(title);
+  container.innerHTML = "\n    <h1>\n      ".concat(newsContent.title, "\n    </h1>\n  \n    <div>\n      <a href=\"#\">\uBAA9\uB85D\uC73C\uB85C</a>\n    <div>\n  ");
 });
+var newsList = [];
+newsList.push("<ul>");
 
 for (var i = 0; i < 10; i++) {
   var div = document.createElement("div");
-  var li = document.createElement("li");
-  var a = document.createElement("a");
-  div.innerHTML = "\n    <li>\n      <a href='#".concat(newsFeed[i].id, "'>\n        ").concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")\n      </a>\n    </li>\n  ");
-  ul.appendChild(div.firstElementChild);
+  newsList.push("\n    <li>\n      <a href='#".concat(newsFeed[i].id, "'>\n        ").concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")\n      </a>\n    </li>\n  "));
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+newsList.push("</ul>");
+container.innerHTML = newsList.join("");
 },{}],"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -175,7 +176,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54664" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59289" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
